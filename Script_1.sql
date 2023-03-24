f6b990607c487d2c1bbd32f3abbf6e696f6a52e1
Yuvaraj Dalavai <yuvaraj_my@Yuvarajs-MacBook-Pro.local>
2023-03-23 23:37:10 -0400

    Automation

--- Script_1.sql
+++ Script_1.sql
@@ -1,3 +1,45 @@
+--CLEANUP SCRIPT
+set serveroutput on
+declare
+    v_table_exists varchar(1) := 'Y';
+    v_sql varchar(2000);
+begin
+   dbms_output.put_line('Start schema cleanup');
+   for i in (select 'LEASED_UNITS' table_name from dual union all
+             select 'REQUEST' table_name from dual union all
+             select 'BUILDING_HAS_AMENITY' table_name from dual union all
+             select 'MAINTAINANCE_PERSONNEL' table_name from dual union all
+             select 'UNIT' table_name from dual union all
+             select 'BUILDING' table_name from dual union all
+             select 'TENANT' table_name from dual union all
+             select 'LEASE' table_name from dual union all
+             select 'AMENITY' table_name from dual union all
+             select 'MANAGEMENT_COMPANY' table_name from dual      
+   )
+   loop
+   dbms_output.put_line('....Drop table '||i.table_name);
+   begin
+       select 'Y' into v_table_exists
+       from USER_TABLES
+       where TABLE_NAME=i.table_name;
+
+       v_sql := 'drop table '||i.table_name;
+       execute immediate v_sql;
+       dbms_output.put_line('........Table '||i.table_name||' dropped successfully');
+       
+   exception
+       when no_data_found then
+           dbms_output.put_line('........Table already dropped');
+   end;
+   end loop;
+   dbms_output.put_line('Schema cleanup successfully completed');
+exception
+   when others then
+      dbms_output.put_line('Failed to execute code:'||sqlerrm);
+end;
+/
+
+
 --CREATE TABLES AS PER DATA MODEL
 CREATE TABLE Management_Company (
   company_id INT PRIMARY KEY,
