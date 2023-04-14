9d9cde5352f5bcb29985483a2a7995b8b4183c0d
Sravani0679 <pillarisetty.s@northeastern.edu>
2023-04-14 02:23:04 -0400

    adding procedure functions

---
+++ procedures_functions.sql
@@ -0,0 +1,85 @@
+CREATE OR REPLACE PROCEDURE get_all_buildings AS
+  v_building_name BUILDING.BUILDING_NAME%TYPE;
+BEGIN
+  -- Cursor to retrieve building_name
+  FOR c_building_name IN (SELECT BUILDING_NAME FROM BUILDING)
+  LOOP
+    v_building_name := c_building_name.BUILDING_NAME;
+    DBMS_OUTPUT.PUT_LINE(v_building_name);
+  END LOOP;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE get_company_buildings (CompanyUsername IN VARCHAR)
+AS
+  companyid NUMBER;
+  building_name building.building_name%TYPE; -- declare variable with the same type as column
+BEGIN
+  SELECT company_id INTO companyid FROM management_company WHERE lower(company_username) = lower(CompanyUsername);
+  -- use a cursor to retrieve the building names
+  FOR building_rec IN (SELECT building_name FROM building WHERE company_id = companyid) LOOP
+    building_name := building_rec.building_name;
+    DBMS_OUTPUT.PUT_LINE(building_name); -- print the building name
+  END LOOP;
+END;
+/
+
+
+CREATE OR REPLACE PROCEDURE get_building_details (buildingname IN VARCHAR)
+AS
+  address building.address%TYPE;
+  zipcode building.zipcode%TYPE;
+  number_of_floors building.number_of_floors%TYPE;
+  parking_spots building.parking_spots%TYPE;
+  type_of_building building.type_of_building%TYPE;
+BEGIN
+  SELECT address, zipcode, number_of_floors, parking_spots, type_of_building
+  INTO address, zipcode, number_of_floors, parking_spots, type_of_building
+  FROM building
+  WHERE building_name = buildingname;
+
+  DBMS_OUTPUT.PUT_LINE('Address: ' || address);
+  DBMS_OUTPUT.PUT_LINE('Zipcode: ' || zipcode);
+  DBMS_OUTPUT.PUT_LINE('Number of floors: ' || number_of_floors);
+  DBMS_OUTPUT.PUT_LINE('Parking spots: ' || parking_spots);
+  DBMS_OUTPUT.PUT_LINE('Type of building: ' || type_of_building);
+END;
+/
+
+
+CREATE OR REPLACE FUNCTION get_tenants(
+    unitno IN NUMBER
+)
+RETURN VARCHAR2
+DETERMINISTIC
+IS
+    tenantname VARCHAR2(50);
+    tenantid NUMBER;
+BEGIN
+    SELECT tenant_id INTO tenantid 
+    FROM leased_units il 
+    LEFT JOIN unit u ON il.unit_no = u.unit_no
+    WHERE il.unit_no = unitno;
+
+    SELECT first_name||' '||last_name|| ' and Phone Number is ' ||Phone_number INTO tenantname 
+    FROM tenant t 
+    WHERE t.tenant_id = tenantid;
+
+    RETURN tenantname;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE get_maintenance_requests(
+  unitno IN NUMBER
+)
+IS
+  description VARCHAR2(200);
+  status VARCHAR2(200);
+BEGIN
+  SELECT request_description, request_status INTO description, status
+  FROM request r 
+  WHERE r.unit_no = unitno;
+  
+  DBMS_OUTPUT.PUT_LINE('Description: ' || description || ', Status: ' || status);
+END;
+/
